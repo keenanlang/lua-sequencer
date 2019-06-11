72ae32fad58e7dd429b6cfd6499a30dbb46019f1
klang <klang@aps.anl.gov>
2019-06-11 17:42:50 -0500

    use double bracket to show multi-line

--- iocBoot/iocTest/seqTest.lua
+++ iocBoot/iocTest/seqTest.lua
@@ -3,13 +3,17 @@ VAL = 1
 test = seq.program "seqtest" {
 	state1=
 	{
-		seq.transition("VAL==1", "VAL = 0; print('Hello,')", "state2")
+		seq.transition("VAL==1", 
+		[[
+			VAL = 0
+			print("Hello,")
+		]], "state2")
 	},
 	
 	state2=
 	{
-		seq.transition("VAL==0", "print('World')", "exit"),
-		seq.transition("VAL==1", "print('Failure')", "exit")
+		seq.transition("VAL==0", [[ print("World") ]], "exit"),
+		seq.transition("VAL==1", [[ print("Failure") ]], "exit")
 	}
 }
 
